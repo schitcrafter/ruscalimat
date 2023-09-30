75202a6b104b9e108d218b19b58c62d4de92666c
schitcrafter <vapor.schitcrafter@gmail.com>
2023-09-30 22:22:19 +0200

    🐛 Fix config bug

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -5,12 +5,14 @@ pub static SETTINGS: Lazy<Config> = Lazy::new(setup);
 
 fn setup() -> Config {
     let builder = Config::builder().add_source(config::File::with_name("config"));
-    let builder = builder.add_source(config::Environment::with_prefix("APP").separator("_"));
     #[cfg(debug_assertions)]
     let builder = builder.add_source(config::File::with_name("config.dev").required(false));
 
     #[cfg(not(debug_assertions))]
     let builder = builder.add_source(config::File::with_name("config.prod"));
 
-    builder.build().unwrap()
+    builder
+        .add_source(config::Environment::with_prefix("APP").separator("_"))
+        .build()
+        .unwrap()
 }
